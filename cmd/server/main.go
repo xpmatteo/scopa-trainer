b54c87375c1af3ab34e0922ddbd19288e7407132
Matteo Vaccari <mvaccari@thoughtworks.com>
2025-03-03 17:37:54 +0100

    Unexcluded main directory

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -21,7 +21,6 @@ func main() {
 	// Set up routes
 	http.HandleFunc("/", handler.HandleIndex)
 	http.HandleFunc("/new-game", handler.HandleNewGame)
-
 	// Start the server
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
